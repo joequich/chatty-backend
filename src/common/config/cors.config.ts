204bb75693e5370cc862402372338b136e882ba5
joequich <joequich@gmail.com>
2025-07-18 20:37:19 -0500

    chore: expose app to any host in local network

--- src/common/config/cors.config.ts
+++ src/common/config/cors.config.ts
@@ -1,5 +1,8 @@
 const config = {
-  origin: [/^https?:\/\/(localhost|127.0.0.1|10.0.2.2)(:\d{0,4})?$/],
+  origin: [
+    /^https?:\/\/(localhost|127.0.0.1|10.0.2.2)(:\d{0,4})?$/,
+    /^https?:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d{1,5})?$/,
+  ],
   credentials: true,
   allowedHeaders: ['content-type', 'authorization', 'origin'],
 };
